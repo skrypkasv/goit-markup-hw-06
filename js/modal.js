b951a874628b3b58299f9023fd4e968325126cf1
skrypkasv <skrypkasv@gmail.com>
2020-07-02 11:43:22 +0300

    Fixes js files

--- js/modal.js
+++ js/modal.js
@@ -12,3 +12,14 @@
     refs.backdrop.classList.toggle('is-hidden');
   }
 })();
+(() => {
+  document.querySelector('.js-modal-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`),
+    );
+
+    e.currentTarget.reset();
+  });
+})();
